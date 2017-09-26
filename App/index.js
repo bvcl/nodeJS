fa53f66b3e4a5e4c82ca057c9590ee6bd35adcc3
bvcl <bvcl@cin.ufpe.br>
2017-09-26 06:45:14 -0300

    apresentacao final

---
+++ App/index.js
@@ -0,0 +1,21 @@
+const port = 3000
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, '../views/layouts')
+}))
+
+app.get('/', (request, response) => {
+  response.render('problema3', {})
+})
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, '../views'))
+
+app.listen(port)
